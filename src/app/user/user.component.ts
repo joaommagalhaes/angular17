dfad7e890a7a41c6d2fe44de9aff1afdc2da4fea
joaomagalhaes <joao.m.magalhaes@wit-software.com>
2024-10-30 16:42:55 +0000

    user commponent, use of angular signals

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[0]);
+
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  onSelectUser() {
+    console.log('click');
+  }
+}
